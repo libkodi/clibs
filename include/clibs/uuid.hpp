a6dc70ff66f2eeeb8344ed49cdac4b61170b4c7c
libkodi <nowtd123@gmail.com>
2021-12-17 15:31:43 +0800

    update windows uuid

--- include/clibs/uuid.hpp
+++ include/clibs/uuid.hpp
@@ -6,7 +6,8 @@
 #include <sstream>
 
 #ifdef _WIN32
-#include <windows.h>
+#include <objbase.h>
+#pragma comment(lib, "ole32.lib")
 #else
 #include <uuid/uuid.h>
 #endif
@@ -14,21 +15,28 @@
 namespace clibs {
     std::string uuid() {
         std::string res;
+        char uuid_cstr[37];
 #ifdef _WIN32
-        UuidCreate(&uuid);
-        char* uuid_cstr = nullptr;
-        UuidToStringA(&uuid, reinterpret_cast<RPC_CSTR*>(&uuid_cstr));
-        res = std::string(uuid_cstr);
-        RpcStringFreeA(reinterpret_cast<RPC_CSTR*>(&uuid_cstr));
+        GUID uuid;
+        CoCreateGuid(&uuid);
+        sprintf(uuid_cstr, "%08x-", uuid.Data1);
+        sprintf(uuid_cstr + 9, "%04x-", uuid.Data2);
+        sprintf(uuid_cstr + 14, "%04x-", uuid.Data3);
+        sprintf(uuid_cstr + 19, "%02x", uuid.Data4[0]);
+        sprintf(uuid_cstr + 21, "%02x-", uuid.Data4[1]);
+        sprintf(uuid_cstr + 24, "%02x", uuid.Data4[2]);
+        sprintf(uuid_cstr + 26, "%02x", uuid.Data4[3]);
+        sprintf(uuid_cstr + 28, "%02x", uuid.Data4[4]);
+        sprintf(uuid_cstr + 30, "%02x", uuid.Data4[5]);
+        sprintf(uuid_cstr + 32, "%02x", uuid.Data4[6]);
+        sprintf(uuid_cstr + 34, "%02x", uuid.Data4[7]);
+        uuid_cstr[36] = '\0';
 #else
         uuid_t uid;
-        char uuid_cstr[36];
         uuid_generate(uid);
         uuid_unparse(uid, uuid_cstr);
-        res = std::string(uuid_cstr);
 #endif
-        
-        return res;
+        return std::string(uuid_cstr);        
     }
 }
 
